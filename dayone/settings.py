7ffb1eeef25bc052da908a4e8b694a643f9bfd16
arjun867 <sammengiarjun@gmail.com>
2023-12-30 18:39:15 +0530

    hiding secret key

--- dayone/settings.py
+++ dayone/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,10 +29,10 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e2p8a1ki7=lqas9#j)c&@5-hce)f0^qd1-(3s7b!ekr^!yh-m6'
+SECRET_KEY=os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
